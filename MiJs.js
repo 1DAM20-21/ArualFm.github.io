23d445c64cc4d0653fdd7d5bbeee88911c2dbe1b
ArualFM <75784416+ArualFM@users.noreply.github.com>
2021-05-13 12:54:57 +0200

    Update MiJs.js

--- MiJs.js
+++ MiJs.js
@@ -1,8 +1,10 @@
-'use strict';
-
-loadLDocA("libros.xml","xml");
+'use strict;'
+loadDocA("libros.xml","xml");
 
 function gestionarFicheroXML(fichero)
 {
-    alert("Llegue");
+    alert(xmlDoc.getElementsByTagName('libro')[0].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('libro')[0].childNodes[1].firstChild.nodeValue)
+
+	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent + "</b>"
 }
