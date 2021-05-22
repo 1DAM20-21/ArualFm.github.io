5c2c736c00338e0706a0084d10fe016f20e66a0d
ArualFM <laura.fernandezmol@iesdoctorbalmis.com>
2021-05-22 14:15:59 +0200

    Update MiJs.js

--- MiJs.js
+++ MiJs.js
@@ -5,7 +5,7 @@ function gestionarFicheroXML(libros) {
     alert(xmlDoc.getElementsByTagName('libros')[0].firstChild.nodeValue)
 	alert(xmlDoc.getElementsByTagName('libros')[0].childNodes[1].firstChild.nodeValue)
 
-	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent + "</b>"
+	document.getElementById('libros').innerHTML += "<b>" + xmlDoc.querySelector('libros:nth-child(2)').textContent + "</b>"
 }
 document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
 
