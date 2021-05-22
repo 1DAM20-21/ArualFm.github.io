af8c4cd63ca5fea15ce81f7b5d88468d7a2d0d6e
ArualFM <laura.fernandezmol@iesdoctorbalmis.com>
2021-05-22 17:14:06 +0200

    Update MiJs.js

--- MiJs.js
+++ MiJs.js
@@ -1,18 +1,16 @@
 'use strict'
 
-let fichero = "libros.xml";
-loadLDocA(fichero,"xml")
-function gestionarFicheroXML(document){
+function gestionarFicheroXML(xmlDoc){
 	
-	alert(xmlDoc.getElementsByTagName('libro')[0].firstChild.nodeValue)
-	alert(xmlDoc.getElementsByTagName('libreria')[0].childNodes[1].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('libreria')[0].firstChild.nodeValue)
+	alert(xmlDoc.getElementsByTagName('busquedaLibros')[0].childNodes[1].firstChild.nodeValue)
 
-	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('libro:nth-child(2)').textContent + "</b>"
+	document.getElementById('ficheroXML').innerHTML += "<b>" + xmlDoc.querySelector('alumno:nth-child(2)').textContent + "</b>"
 	
 }
 
 document.querySelector("div:nth-child(1)").addEventListener("click",()=>{
 
-    let xmlDoc = loadLDocA("leerFicheroXML.xml","xml");
+    let xmlDoc = loadLDocA("libros.xml","xml");
 })
     
