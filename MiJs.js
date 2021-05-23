7bc5a69b3cc8ef5743a5f6518ec3f39b3accf776
ArualFM <laura.fernandezmol@iesdoctorbalmis.com>
2021-05-23 20:50:23 +0200

    añado la extension de js

--- MiJs.js
+++ MiJs.js
@@ -2,12 +2,11 @@
 
 function gestionarFicheroXML(xmlDoc){
 	let capaVacia = document.querySelector("#ficheroXML")
+	let capaVacia2 = document.querySelector("#ficheroXML")
 	let libros = xmlDoc.querySelectorAll("libreria")
 	for (let i = 0; i < libros.length; i++) {
-		 capaVacia.innerHTML = capaVacia.innerHTML + "<table>" + 
-		 libros[i].textContent + "<tr>" + libros[i+1].textContent +
-		 libros[--libros.length].textContent + "<td>" + "</td>" + 
-		 "</tr>" + "</table>" 
+		capaVacia.innerHTML = capaVacia.innerHTML + "<p>" + alumnos[i].textContent + "</p>"
+
 	}
 }
 
